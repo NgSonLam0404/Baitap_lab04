44420d94c0825441478328bd692e7af0b22a3eb1
NgSonLam0404 <124905175+NgSonLam0404@users.noreply.github.com>
2023-03-22 14:50:27 +0700

    ngay 22/3/2023

---
+++ Bai_thuc_hanh_chuong_5/src/bai2/Hinh.java
@@ -0,0 +1,8 @@
+package bai2;
+
+public class Hinh {
+	public double dienTich() {
+		return 0;
+	}
+	
+}
---
+++ Bai_thuc_hanh_chuong_5/src/bai2/HinhTron.java
@@ -0,0 +1,17 @@
+package bai2;
+
+public class HinhTron extends Hinh {
+	private float r;
+	private double Pi=3.14;
+	public HinhTron(float r) {
+		this.r=r;
+	}
+	@Override
+	public String toString() {
+		return "HinhTron [r=" + r + "]";
+	}
+	public double dienTich() {
+		return Pi*this.r;
+	}
+
+}
