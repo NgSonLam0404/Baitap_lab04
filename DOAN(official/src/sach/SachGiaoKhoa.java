e893c62a4ac4bf93d223b83e894b35b9505ac16b
NgSonLam0404 <124905175+NgSonLam0404@users.noreply.github.com>
2023-04-11 21:30:49 +0700

    code ne

---
+++ DOAN(official/src/sach/SachGiaoKhoa.java
@@ -0,0 +1,121 @@
+package sach;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.Scanner;
+
+public abstract class SachGiaoKhoa implements Comparable<SachGiaoKhoa>,Serializable {
+	protected String maSach,tenSach, nhaXuatBan,loaiSach, lop;
+	protected Date ngayXuatBan, ngayNhapKho;
+	protected int soLanTaiBan, soLuong;
+
+	public SachGiaoKhoa(String maSach,String tenSach, String nhaXuatBan,String loaiSach,String lop, Date ngayXuatBan, Date ngayNhapKho, int soLanTaiBan,
+			int soLuong) {
+		this.maSach = maSach;
+		this.tenSach=tenSach;
+		this.nhaXuatBan = nhaXuatBan;
+		this.loaiSach=loaiSach;
+		this.lop=lop;
+		this.ngayXuatBan = ngayXuatBan;
+		this.ngayNhapKho = ngayNhapKho;
+		this.soLanTaiBan = soLanTaiBan;
+		this.soLuong = soLuong;
+	}
+	public SachGiaoKhoa(String maSach) {
+		this.maSach = maSach;
+	}
+
+
+	public SachGiaoKhoa() {
+	}
+
+	public String getMaSach() {
+		return maSach;
+	}
+
+	public void setMaSach(String maSach) {
+		this.maSach = maSach;
+	}
+
+	public String getNhaXuatBan() {
+		return nhaXuatBan;
+	}
+	public void setTenSach(String tenSach) {
+		this.tenSach = tenSach;
+	}
+	public String getTenSach() {
+		return tenSach;
+	}
+	public void setNhaXuatBan(String nhaXuatBan) {
+		this.nhaXuatBan = nhaXuatBan;
+	}
+
+	public Date getNgayXuatBan() {
+		return ngayXuatBan;
+	}
+	public String getLoaiSach() {
+		return loaiSach;
+	}
+	public void setLoaiSach(String loaiSach) {
+		this.loaiSach=loaiSach;
+	}
+	public String getLop() {
+		return lop;
+	}
+	public void setLop(String lop) {
+		this.lop=lop;
+	}
+	public void setNgayXuatBan(Date ngayXuatBan) {
+		this.ngayXuatBan = ngayXuatBan;
+	}
+
+	public Date getNgayNhapKho() {
+		return ngayNhapKho;
+	}
+
+	public void setNgayNhapKho(Date ngayNhapKho) {
+		this.ngayNhapKho = ngayNhapKho;
+	}
+
+	public int getSoLanTaiBan() {
+		return soLanTaiBan;
+	}
+
+	public void setSoLanTaiBan(int soLanTaiBan) {
+		this.soLanTaiBan = soLanTaiBan;
+	}
+
+	public int getSoLuong() {
+		return soLuong;
+	}
+
+	public void setSoLuong(int soLuong) {
+		this.soLuong = soLuong;
+	}
+
+	public void nhap() {
+		
+	}
+
+	public abstract boolean kiemTraSachMoi();
+	public  void chinhsuathongtin() {};
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SachGiaoKhoa other = (SachGiaoKhoa) obj;
+		return Objects.equals(maSach, other.maSach);
+	}
+
+	@Override
+	public String toString() {
+		return "SachGiaoKhoa [maSach:" + maSach+ ", ten sach: "+tenSach + ", loai sach: "+loaiSach+ ", lop: "+lop+ ", nhaXuatBan: " + nhaXuatBan + ", ngayXuatBan:"
+				+ ngayXuatBan.toString() + ", ngayNhapKho: " + ngayNhapKho.toString() + ", soLanTaiBan: " + soLanTaiBan
+				+ ", soLuong=" + soLuong + "]";
+	}
+
+}
---
+++ DOAN(official/src/sach/Test.java
@@ -0,0 +1,8 @@
+package sach;
+
+public class Test {
+	public static void main(String[] args) {
+		Menu menu = new Menu();
+		menu.menu();
+	}
+}
